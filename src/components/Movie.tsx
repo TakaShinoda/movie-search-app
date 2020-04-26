d902daa097b02344a3aaa84dadff7def0e0b252c
Takahiro <tkhr_621@docomo.ne.jp>
2020-04-27 02:33:22 +0900

    jsx -> tsx

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const DEFAULT_PLACEHOLDER_IMAGE = "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg"
 
-export const Movie = ({ movie }) => {
+interface Props {
+    movie: {
+        Poster: string
+        Title: string
+        Year: string
+    }
+}
+
+export const Movie: React.FC<Props> = ({ movie }) => {
     const poster = movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster
 
     return (
